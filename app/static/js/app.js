d94cd6dcc10fe0876b67cd5a9eb346f8ce8befcd
Kayon-Marie <k.marie.douglas@hotmail.com>
2019-03-20 09:50:30 -0400

    News-list component created, api key generated and titles from the articles display

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -39,6 +39,33 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('news-list', {
+    template: `
+        <div class="news">
+            <h2>News</h2>
+            <ul class="news__list">
+            <li v-for="article in articles" class="news__item">{{ article.title }}</li>
+            </ul>
+        </div>
+    `,
+    created: function() {
+      let self = this;
+      fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<insert api key>')
+        .then(function(response) {
+            return response.json();
+        })           
+        .then(function(data) {             
+          console.log(data);
+          self.articles = data.articles;           
+        });     
+    },
+    data: function() {
+      return {
+        articles: []
+      }
+    }
+})
+
 
 let app = new Vue({
     el: '#app',
